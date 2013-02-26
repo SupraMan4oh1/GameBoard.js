30925160fde15d5eea0db03b7497660d93179643
Your Name Here <Brendan.Conron@tufts.edu>
2013-02-25 21:46:03 -0500

    Refactored code to closure form

--- GameBoard.js
+++ GameBoard.js
@@ -46,266 +46,117 @@ var GameBoard = (function(){
 		var setCanvasWidth = function(w, percent){
 		percent ? width*=w : width = w;
 		canvas.width = width;
-	};
-	var setCanvasHeight = function(h, percent){
-		percent ? height = h*height : height = h;
-		canvas.height = height;
-	};
-	this.setWidthBlocks = function(wb){
-		widthBlocks = wb;
-	};
-	this.setHeightBlocks = function(hb){
-		heightBlocks = hb;
-	};
-	/*variables and methods relating to the number of pixels per block*/
-	var xPixelsPerBlock = this.getCanvasWidth()/this.getWidthBlocks();
-	var yPixelsPerBlock = this.getCanvasHeight()/this.getHeightBlocks();
-	this.getXPixelsPerBlock = function(){
-		return xPixelsPerBlock;
-	};
-	this.getYPixelsPerBlock = function(){
-		return yPixelsPerBlock;
-	};
-	/*Resize the canvas. X and Y are new dimensions in pixels. 
-	* If percent == true, X and Y are treated as percents and the
-	* the canvas will be resized based on those*/
-	this.resizeCanvas = function(w, h, percent){
-		setCanvasWidth(w, percent);
-		setCanvasHeight(h, percent);
-	};
-	/*Takes in an x and y coordinate. Returns 
-	* block coordinate object*/
-	this.getBlockPosition = function(x, y){
-		return new Block(Math.floor(xPixelsPerBlock), Math.floor(yPixelsPerBlock));
-	};
-	/*Pass in x,y coordinates of two objects (or future positions)
-	 * to check to see if they exist in the same block. If yes, then
-	 * collision.
-	 * NOTE: Only checks for block to block collision. For
-	 * collisions within blocks based on pixels and object widths, well
-	 * wait for verision 1.1.*/
-	this.checkBlockCollision = function(x1, y1, x2, y2){
-		var block1 = that.getBlockPosition(x1, y1);
-		var block2 = that.getBlockPosition(x2, y2);
-		if(block1.getXBlock() === block2.getXBlock() && block1.getYBlock === block2.getYBlock)
-			return true;
-		return false;
-	};
-	/*Coordinates of two objects in the same block. Note, 
-	* these are the coordinates of the top left corner,
-	* not the center of the image 
-	*/
-	this.checkPixelCollision = function(canvasObj1, canvasObj2){
-		if(doIntersectX(canvasObj1.getObjX(), canvasObj2.getObjX(), canvasObj1.getObjWidth(), 
-			canvasObj2.getObjWidth()) && doIntersectY(canvasObj1.getObjY(), canvasObj2.getObjY(), canvasObj1.getObjHeight(), canvasObj2.getObjHeight())) return true;
-		return false;
-	};
-		/*private helper function*/
-	var doIntersectX = function(x1, x2, obj1W, obj2W){
-		if((x2 >= x1) && (x2 <= (x1+obj1W))) return true;
-		if((x1 >= x2) && (x1 <= (x2+obj2W))) return true; return false;
-	};
-	var doIntersectY = function(y1, y2, obj1H, obj2H){
-		if((y2 >= y1) && (y2 <= (y1+obj1H))) return true;
-		if((y1 >= y2) && (y1 <= (y2+obj2H))) return true;
-		return false;
-	};
-	this.Block = function(x, y){
-		var xBlock = x;
-		var yBlock = y;
-		/*Use ranges for more control in custom collision detecting*/ 
-		var lowX = that.xPixelsPerBlock*this.getXBlock();
-		var highX = that.xPixelsPerBlock*(this.getXBlock()+1);
-		var lowY = that.yPixelsPerBlock*this.getYBlock();
-		var highY = that.yPixelsPerBlock*(this.getYBlock()+1);
-		/*Sets x and y ranges of coordinates based 
-		* on block coordinates*/
-		this.getXBlock = function(){
-			return xBlock;
 		};
-		this.getYBlock = function(){
-			return yBlock;
+		var setCanvasHeight = function(h, percent){
+			percent ? height = h*height : height = h;
+			canvas.height = height;
 		};
-		this.getLowX = function(){
-			return lowX;
+		this.setWidthBlocks = function(wb){
+			widthBlocks = wb;
 		};
-		this.getHighX = function(){
-			return highX;
+		this.setHeightBlocks = function(hb){
+			heightBlocks = hb;
 		};
-		this.getLowY = function(){
-			return lowY;
+		/*variables and methods relating to the number of pixels per block*/
+		var xPixelsPerBlock = this.getCanvasWidth()/this.getWidthBlocks();
+		var yPixelsPerBlock = this.getCanvasHeight()/this.getHeightBlocks();
+		this.getXPixelsPerBlock = function(){
+			return xPixelsPerBlock;
 		};
-		this.getHighY = function(){
-			return highY;
+		this.getYPixelsPerBlock = function(){
+			return yPixelsPerBlock;
 		};
-	};
-	this.CanvasObj = function(x, y, w, h){
-		var xCoord = x;
-		var yCoord = y;
-		var width = w;
-		var height = h;
-		this.getObjX = function(){
-			return xCoord;
+		/*Resize the canvas. X and Y are new dimensions in pixels. 
+		* If percent == true, X and Y are treated as percents and the
+		* the canvas will be resized based on those*/
+		this.resizeCanvas = function(w, h, percent){
+			setCanvasWidth(w, percent);
+			setCanvasHeight(h, percent);
 		};
-		this.getObjY = function(){
-			return yCoord;
+		/*Takes in an x and y coordinate. Returns 
+		* block coordinate object*/
+		this.getBlockPosition = function(x, y){
+			return new Block(Math.floor(xPixelsPerBlock), Math.floor(yPixelsPerBlock));
 		};
-		this.getObjWidth = function(){
-			return width;
-		}
-		this.getObjHeight = function(){
-			return height;
+		/*Pass in x,y coordinates of two objects (or future positions)
+		 * to check to see if they exist in the same block. If yes, then
+		 * collision.
+		 * NOTE: Only checks for block to block collision. For
+		 * collisions within blocks based on pixels and object widths, well
+		 * wait for verision 1.1.*/
+		this.checkBlockCollision = function(x1, y1, x2, y2){
+			var block1 = that.getBlockPosition(x1, y1);
+			var block2 = that.getBlockPosition(x2, y2);
+			if(block1.getXBlock() === block2.getXBlock() && block1.getYBlock === block2.getYBlock)
+				return true;
+			return false;
 		};
-	};
-})();
-
-
-function GameBoard(can, x, y){
-	this.context = can.getContext("2d");
-	var canvas = can;
-	var height = can.height;
-	var width = can.width;
-	var widthBlocks = x;
-	var heightBlocks = y;
-	var totalBlocks = x*y;
-	this.img = new Image();
-	var that = this;
-	/*Array for end-user to modify as they please. Typically
-	* used to store the board as a collection of Block objects.
-	*/ 
-	this.blocksBoard = [[]];
-	/*getters*/
-	this.isCanvasSupported = function(){
-		return canvas.getContext ? true : false;
-	};
-	this.getCanvas = function(){
-		return canvas;
-	};
-	this.getCanvasHeight = function(){
-		return height;
-	};
-	this.getCanvasWidth = function(){
-		return width;
-	};
-	this.getWidthBlocks = function(){
-		return widthBlocks;
-	};
-	this.getHeightBlocks = function(){
-		return heightBlocks;
-	};
-	/*setters and private helper functions*/
-	var setCanvasWidth = function(w, percent){
-		percent ? width*=w : width = w;
-		canvas.width = width;
-	};
-	var setCanvasHeight = function(h, percent){
-		percent ? height = h*height : height = h;
-		canvas.height = height;
-	};
-	this.setWidthBlocks = function(wb){
-		widthBlocks = wb;
-	};
-	this.setHeightBlocks = function(hb){
-		heightBlocks = hb;
-	};
-	/*variables and methods relating to the number of pixels per block*/
-	var xPixelsPerBlock = this.getCanvasWidth()/this.getWidthBlocks();
-	var yPixelsPerBlock = this.getCanvasHeight()/this.getHeightBlocks();
-	this.getXPixelsPerBlock = function(){
-		return xPixelsPerBlock;
-	};
-	this.getYPixelsPerBlock = function(){
-		return yPixelsPerBlock;
-	};
-	/*Resize the canvas. X and Y are new dimensions in pixels. 
-	* If percent == true, X and Y are treated as percents and the
-	* the canvas will be resized based on those*/
-	this.resizeCanvas = function(w, h, percent){
-		setCanvasWidth(w, percent);
-		setCanvasHeight(h, percent);
-	};
-	/*Takes in an x and y coordinate. Returns 
-	* block coordinate object*/
-	this.getBlockPosition = function(x, y){
-		return new Block(Math.floor(xPixelsPerBlock), Math.floor(yPixelsPerBlock));
-	};
-	/*Pass in x,y coordinates of two objects (or future positions)
-	 * to check to see if they exist in the same block. If yes, then
-	 * collision.
-	 * NOTE: Only checks for block to block collision. For
-	 * collisions within blocks based on pixels and object widths, well
-	 * wait for verision 1.1.*/
-	this.checkBlockCollision = function(x1, y1, x2, y2){
-		var block1 = that.getBlockPosition(x1, y1);
-		var block2 = that.getBlockPosition(x2, y2);
-		if(block1.getXBlock() === block2.getXBlock() && block1.getYBlock === block2.getYBlock)
-			return true;
-		return false;
-	};
-	/*Coordinates of two objects in the same block. Note, 
-	* these are the coordinates of the top left corner,
-	* not the center of the image 
-	*/
-	this.checkPixelCollision = function(canvasObj1, canvasObj2){
-		if(doIntersectX(canvasObj1.getObjX(), canvasObj2.getObjX(), canvasObj1.getObjWidth(), 
-			canvasObj2.getObjWidth()) && doIntersectY(canvasObj1.getObjY(), canvasObj2.getObjY(), canvasObj1.getObjHeight(), canvasObj2.getObjHeight())) return true;
-		return false;
-	};
-		/*private helper function*/
-	var doIntersectX = function(x1, x2, obj1W, obj2W){
-		if((x2 >= x1) && (x2 <= (x1+obj1W))) return true;
-		if((x1 >= x2) && (x1 <= (x2+obj2W))) return true; return false;
-	};
-	var doIntersectY = function(y1, y2, obj1H, obj2H){
-		if((y2 >= y1) && (y2 <= (y1+obj1H))) return true;
-		if((y1 >= y2) && (y1 <= (y2+obj2H))) return true;
-		return false;
-	};
-	this.Block = function(x, y){
-		var xBlock = x;
-		var yBlock = y;
-		/*Use ranges for more control in custom collision detecting*/ 
-		var lowX = that.xPixelsPerBlock*this.getXBlock();
-		var highX = that.xPixelsPerBlock*(this.getXBlock()+1);
-		var lowY = that.yPixelsPerBlock*this.getYBlock();
-		var highY = that.yPixelsPerBlock*(this.getYBlock()+1);
-		/*Sets x and y ranges of coordinates based 
-		* on block coordinates*/
-		this.getXBlock = function(){
-			return xBlock;
+		/*Coordinates of two objects in the same block. Note, 
+		* these are the coordinates of the top left corner,
+		* not the center of the image 
+		*/
+		this.checkPixelCollision = function(canvasObj1, canvasObj2){
+			if(doIntersectX(canvasObj1.getObjX(), canvasObj2.getObjX(), canvasObj1.getObjWidth(), 
+				canvasObj2.getObjWidth()) && doIntersectY(canvasObj1.getObjY(), canvasObj2.getObjY(), canvasObj1.getObjHeight(), canvasObj2.getObjHeight())) return true;
+			return false;
 		};
-		this.getYBlock = function(){
-			return yBlock;
+			/*private helper function*/
+		var doIntersectX = function(x1, x2, obj1W, obj2W){
+			if((x2 >= x1) && (x2 <= (x1+obj1W))) return true;
+			if((x1 >= x2) && (x1 <= (x2+obj2W))) return true; return false;
 		};
-		this.getLowX = function(){
-			return lowX;
+		var doIntersectY = function(y1, y2, obj1H, obj2H){
+			if((y2 >= y1) && (y2 <= (y1+obj1H))) return true;
+			if((y1 >= y2) && (y1 <= (y2+obj2H))) return true;
+			return false;
 		};
-		this.getHighX = function(){
-			return highX;
+		this.Block = function(x, y){
+			var xBlock = x;
+			var yBlock = y;
+			/*Use ranges for more control in custom collision detecting*/ 
+			var lowX = that.xPixelsPerBlock*this.getXBlock();
+			var highX = that.xPixelsPerBlock*(this.getXBlock()+1);
+			var lowY = that.yPixelsPerBlock*this.getYBlock();
+			var highY = that.yPixelsPerBlock*(this.getYBlock()+1);
+			/*Sets x and y ranges of coordinates based 
+			* on block coordinates*/
+			this.getXBlock = function(){
+				return xBlock;
+			};
+			this.getYBlock = function(){
+				return yBlock;
+			};
+			this.getLowX = function(){
+				return lowX;
+			};
+			this.getHighX = function(){
+				return highX;
+			};
+			this.getLowY = function(){
+				return lowY;
+			};
+			this.getHighY = function(){
+				return highY;
+			};
 		};
-		this.getLowY = function(){
-			return lowY;
-		};
-		this.getHighY = function(){
-			return highY;
+		this.CanvasObj = function(x, y, w, h){
+			var xCoord = x;
+			var yCoord = y;
+			var width = w;
+			var height = h;
+			this.getObjX = function(){
+				return xCoord;
+			};
+			this.getObjY = function(){
+				return yCoord;
+			};
+			this.getObjWidth = function(){
+				return width;
+			}
+			this.getObjHeight = function(){
+				return height;
+			};
 		};
 	};
-	this.CanvasObj = function(x, y, w, h){
-		var xCoord = x;
-		var yCoord = y;
-		var width = w;
-		var height = h;
-		this.getObjX = function(){
-			return xCoord;
-		};
-		this.getObjY = function(){
-			return yCoord;
-		};
-		this.getObjWidth = function(){
-			return width;
-		}
-		this.getObjHeight = function(){
-			return height;
-		};
-	};
-}
+	return constr;
+}());
